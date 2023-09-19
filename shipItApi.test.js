22204b07651507e86ac96bea014d13dbdf512f44
Brianna Tran <briannatran45@gmail.com>
2023-09-19 16:57:42 -0700

    mocked out fetch request

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,11 +1,20 @@
 "use strict";
 
-const {
-  shipProduct,
-} = require("./shipItApi");
-
+const { shipProduct, SHIPIT_SHIP_URL } = require("./shipItApi");
+const fetchMock = require('fetch-mock');
 
 test("shipProduct", async function () {
+  fetchMock.post(SHIPIT_SHIP_URL, {
+    receipt: {
+      itemId: 1000,
+      name: 'Test Tester',
+      addr: '100 Test St',
+      zip: '12345-6789',
+      shipId: 1225
+    },
+    status: 200
+  });
+
   const shipId = await shipProduct({
     productId: 1000,
     name: "Test Tester",
@@ -13,5 +22,5 @@ test("shipProduct", async function () {
     zip: "12345-6789",
   });
 
-  expect(shipId).toEqual(expect.any(Number));
+  expect(shipId).toEqual(1225);
 });
